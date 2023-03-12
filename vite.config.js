05c4461f84c9ff96e09bfaeb91e884e1c48b477b
OctopusGamer <michaeldiazcampo@gmail.com>
2023-03-12 17:57:56 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/intro-section-with-dropdown/"
+  base: "/Intro-section-with-dropdown/"
 })
